82f2194c1e62bceb1ab418eb0c6ff907599edfdb
frankemon <frankie.hugo@hotmail.com>
2021-04-15 11:50:47 +0200

    add useState for displaying connection message in browser

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,22 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import { useEffect, userEffect } from "react";
+import { useEffect, useState } from "react";
 
 let websocket; // Saves our websocket objects
 
 export default function Home() {
+  useEffect(() => {
+    // Connection, empty array because we arent relying on any variables atm
+    connectToWebSocket();
+  }, []);
+
   function connectToWebSocket() {
     const url = "ws://localhost:5555"; // Note this is ws, not https. Can also be wss for more security, but server requires certificates
     websocket = new WebSocket(url); // Connects and saves url to websocket
 
     websocket.onopen = () => {
       console.log("Websocket connection succesful"); // Connection succesful message goes here
+      setConnected(true);
     };
 
     websocket.onmessage = () => {
@@ -19,6 +25,7 @@ export default function Home() {
 
     websocket.onclose = () => {
       console.log("Websocket connection closed"); // Can add the call to connection again, in an attempt to reconnect
+      setConnected(false);
     };
 
     websocket.onerror = () => {
@@ -26,10 +33,11 @@ export default function Home() {
     };
   }
 
-  useEffect(() => {
-    // Connection, empty array because we arent relying on any variables atm
-    connectToWebSocket();
-  }, []);
+  const [connected, setConnected] = useState(false);
 
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      {connected ? "Connected" : "Offline"}
+    </div>
+  );
 }
